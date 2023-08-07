91263c3005dcbd788baf6d4b25176c1cb13bd0df
Artem Shubovych <shybovycha@gmail.com>
2023-08-07 19:57:37 +1000

    Change pins to the ones with no conflicts

--- keyboards/handwired/dactyl_manuform/5x6_stm32/matrix.c
+++ keyboards/handwired/dactyl_manuform/5x6_stm32/matrix.c
@@ -32,22 +32,25 @@ __attribute__((weak)) void matrix_scan_kb(void) {
   matrix_scan_user();
 }
 
+// "cols": ["B12", "B13", "B14", "B15", "A8", "A15"],
+// "rows": ["A7", "A6", "A5", "A1", "A3", "A2"]
+
 static void init_cols(void) {
-  // "cols": ["A8", "A9", "A10", "A11", "A12", "A15"],
+  // "cols": ["B12", "B13", "B14", "B15", "A8", "A15"],
+  palSetPadMode(GPIOB, 12, PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(GPIOB, 13, PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(GPIOB, 14, PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(GPIOB, 15, PAL_MODE_INPUT_PULLUP);
   palSetPadMode(GPIOA, 8, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOA, 9, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOA, 10, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOA, 11, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOA, 12, PAL_MODE_INPUT_PULLUP);
   palSetPadMode(GPIOA, 15, PAL_MODE_INPUT_PULLUP);
 }
 
 static void init_rows(void) {
-  // "rows": ["A7", "A6", "A5", "A4", "A3", "A2"]
+  // "rows": ["A7", "A6", "A5", "A1", "A3", "A2"]
   palSetPadMode(GPIOA, 7, PAL_MODE_OUTPUT_PUSHPULL);
   palSetPadMode(GPIOA, 6, PAL_MODE_OUTPUT_PUSHPULL);
   palSetPadMode(GPIOA, 5, PAL_MODE_OUTPUT_PUSHPULL);
-  palSetPadMode(GPIOA, 4, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA, 1, PAL_MODE_OUTPUT_PUSHPULL);
   palSetPadMode(GPIOA, 3, PAL_MODE_OUTPUT_PUSHPULL);
   palSetPadMode(GPIOA, 2, PAL_MODE_OUTPUT_PUSHPULL);
 }
@@ -74,23 +77,25 @@ void matrix_init(void) {
   matrix_init_kb();
 }
 
-void matrix_power_up(void) {
-  init_rows();
-  unselect_rows();
-  init_cols();
+// void matrix_power_up(void) {
+//   init_rows();
+//   unselect_rows();
+//   init_cols();
 
-  for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-    matrix[row] = 0;
-  }
-}
+//   for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+//     matrix[row] = 0;
+//   }
+// }
 
 void unselect_rows(void) {
-  GPIOA->ODR =
-    (1 << 9) |
-    (1 << 10) |
-    (1 << 11) |
-    (1 << 12) |
-    (1 << 15);
+  // "rows": ["A7", "A6", "A5", "A1", "A3", "A2"]
+  GPIOA->ODR = 0x00;
+    // (0 << 7) |
+    // (0 << 6) |
+    // (0 << 5) |
+    // (0 << 4) |
+    // (0 << 3) |
+    // (0 << 2);
 }
 
 uint8_t matrix_scan(void) {
@@ -137,22 +142,24 @@ matrix_row_t debounce_mask(matrix_row_t rawcols, uint8_t row) {
 }
 
 matrix_row_t read_cols(uint8_t row) {
-  // "cols": ["A8", "A9", "A10", "A11", "A12", "A15"],
+  // "cols": ["B12", "B13", "B14", "B15", "A8", "A15"],
   if (row < MATRIX_ROWS_PER_SIDE) {
     return 0;
   }
 
-  uint16_t data = GPIOA->IDR;
-  uint16_t bits = (~data);
+  uint16_t portA = GPIOA->IDR;
+  uint16_t portB = GPIOB->IDR;
+  uint16_t bitsA = (~portA);
+  uint16_t bitsB = (~portB);
 
   matrix_row_t result = 0x00;
 
-  if (bits & (1 << 8)) result |= (1);
-  if (bits & (1 << 9)) result |= (1 << 1);
-  if (bits & (1 << 10)) result |= (1 << 2);
-  if (bits & (1 << 11)) result |= (1 << 3);
-  if (bits & (1 << 12)) result |= (1 << 4);
-  if (bits & (1 << 15)) result |= (1 << 5);
+  if (bitsB & (1 << 12)) result |= (1);
+  if (bitsB & (1 << 13)) result |= (1 << 1);
+  if (bitsB & (1 << 14)) result |= (1 << 2);
+  if (bitsB & (1 << 15)) result |= (1 << 3);
+  if (bitsA & (1 << 8)) result |= (1 << 4);
+  if (bitsA & (1 << 15)) result |= (1 << 5);
 
   return ~result;
 }
@@ -175,18 +182,21 @@ matrix_row_t debounce_read_cols(uint8_t row) {
 void matrix_print(void) {}
 
 static void select_row(uint8_t row) {
+  // "rows": ["A7", "A6", "A5", "A1", "A3", "A2"]
   if (row < MATRIX_ROWS_PER_SIDE) {
     // send signal to the other half
   } else {
     if (row == 0)
-      GPIOA->ODR = (1 << 9);
+      GPIOA->ODR = ~(1 << 7);
     else if (row == 1)
-      GPIOA->ODR = (1 << 10);
+      GPIOA->ODR = ~(1 << 6);
     else if (row == 2)
-      GPIOA->ODR = (1 << 11);
+      GPIOA->ODR = ~(1 << 5);
     else if (row == 3)
-      GPIOA->ODR = (1 << 12);
+      GPIOA->ODR = ~(1 << 1);
     else if (row == 4)
-      GPIOA->ODR = (1 << 15);
+      GPIOA->ODR = ~(1 << 3);
+    else if (row == 5)
+      GPIOA->ODR = ~(1 << 2);
   }
 }
