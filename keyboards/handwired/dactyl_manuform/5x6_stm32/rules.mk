64b55c5c63f61295416dab3f648523cf2631766d
Artem Shubovych <shybovycha@gmail.com>
2023-08-13 15:09:43 +1000

    Add Colemak and gaming layouts and a Command mode

--- keyboards/handwired/dactyl_manuform/5x6_stm32/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6_stm32/rules.mk
@@ -1,11 +1,1 @@
-BOOTMAGIC_ENABLE = no   # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no	# Mouse keys
-EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = no	    # Console for debug
-COMMAND_ENABLE = no     # Commands for debug and configuration
-NKRO_ENABLE = yes       # Enable N-Key Rollover
-UNICODE_ENABLE = yes    # Unicode
-ENCODER_ENABLE = no
-OLED_ENABLE = no
-
 SERIAL_DRIVER = usart
